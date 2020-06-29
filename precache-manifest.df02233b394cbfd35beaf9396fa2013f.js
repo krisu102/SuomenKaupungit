5ff74ae84cb7830d7787d3bdf4929b9381ebfa32
krisu <kristian.kyt@gmail.com>
2020-06-29 17:47:08 +0300

    Updates

---
+++ precache-manifest.df02233b394cbfd35beaf9396fa2013f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0000b193c97cb322ab399059f5f9be47",
+    "url": "/SuomenKaupungit/index.html"
+  },
+  {
+    "revision": "6bc97d75edee0c3e8290",
+    "url": "/SuomenKaupungit/static/css/main.67bccd37.chunk.css"
+  },
+  {
+    "revision": "232ffe138b6fcbd3b61b",
+    "url": "/SuomenKaupungit/static/js/2.2e409217.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/SuomenKaupungit/static/js/2.2e409217.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc97d75edee0c3e8290",
+    "url": "/SuomenKaupungit/static/js/main.2d58ffe2.chunk.js"
+  },
+  {
+    "revision": "e86cd8be65a235caa83e",
+    "url": "/SuomenKaupungit/static/js/runtime-main.edad5c77.js"
+  }
+]);
